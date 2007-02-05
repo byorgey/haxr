1be7829831fe69bad0f4d123975785c7a0787d53
bringert <bringert@cs.chalmers.se>
2007-02-05 10:52:23 +0000

    Separated TH and non-TH heterogeneous struct examples.

--- examples/Person.hs
+++ examples/Person.hs
@@ -5,16 +5,18 @@ module Person where
 import Network.XmlRpc.Internals
 
 -- | Record type used to represent the struct in Haskell.
-data Person = Person { name :: String, age :: Int } deriving Show
+data Person = Person { name :: String, age :: Int, spouse :: Maybe String } deriving Show
 
 -- | Converts a Person to and from a heterogeneous struct.
 --   Uses the existing instance of XmlRpcType for [(String,Value)]
 instance XmlRpcType Person where
-    toValue p = toValue [("name",toValue (name p)),
-			 ("age", toValue (age p))]
+    toValue p = toValue $ [("name",toValue (name p)),
+			   ("age", toValue (age p))]
+                           ++ maybe [] ((:[]) . (,) "spouse" . toValue) (spouse p)
     fromValue v = do
 		  t <- fromValue v
 		  n <- getField "name" t
 		  a <- getField "age" t
-		  return Person { name = n, age = a }
+		  s <- getFieldMaybe "spouse" t
+		  return Person { name = n, age = a, spouse = s }
     getType _ = TStruct
