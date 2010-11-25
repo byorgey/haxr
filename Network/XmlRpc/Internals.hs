aea978a7a1cab191d3785422c3d08338316bed6b
Gracjan Polak <gracjanpolak@gmail.com>
2010-11-25 09:55:47 +0000

    Tuple instances, base upgrade, cabal file cleanup

--- Network/XmlRpc/Internals.hs
+++ Network/XmlRpc/Internals.hs
@@ -312,6 +312,39 @@ instance XmlRpcType a => XmlRpcType [(String,a)] where
 		  _ -> typeError v
     getType _ = TStruct
 
+-- Tuple instances may be used for heterogenous array types.
+instance (XmlRpcType a, XmlRpcType b, XmlRpcType c, XmlRpcType d, 
+          XmlRpcType e) => 
+         XmlRpcType (a,b,c,d,e) where
+    toValue (v,w,x,y,z) = 
+        ValueArray [toValue v, toValue w, toValue x, toValue y, toValue z]
+    fromValue (ValueArray [v,w,x,y,z]) = 
+        liftM5 (,,,,) (fromValue v) (fromValue w) (fromValue x) 
+                      (fromValue y) (fromValue z) 
+    fromValue _ = throwError "Expected 5-element tuple!"
+    getType _ = TArray
+
+instance (XmlRpcType a, XmlRpcType b, XmlRpcType c, XmlRpcType d) => 
+         XmlRpcType (a,b,c,d) where
+    toValue (w,x,y,z) = ValueArray [toValue w, toValue x, toValue y, toValue z]
+    fromValue (ValueArray [w,x,y,z]) = 
+        liftM4 (,,,) (fromValue w) (fromValue x) (fromValue y) (fromValue z)
+    fromValue _ = throwError "Expected 4-element tuple!"
+    getType _ = TArray
+
+instance (XmlRpcType a, XmlRpcType b, XmlRpcType c) => XmlRpcType (a,b,c) where
+    toValue (x,y,z) = ValueArray [toValue x, toValue y, toValue z]
+    fromValue (ValueArray [x,y,z]) = 
+        liftM3 (,,) (fromValue x) (fromValue y) (fromValue z)
+    fromValue _ = throwError "Expected 3-element tuple!"
+    getType _ = TArray
+
+instance (XmlRpcType a, XmlRpcType b) => XmlRpcType (a,b) where
+    toValue (x,y) = ValueArray [toValue x, toValue y]
+    fromValue (ValueArray [x,y]) = liftM2 (,) (fromValue x) (fromValue y)
+    fromValue _ = throwError "Expected 2-element tuple."
+    getType _ = TArray
+
 -- | Get a field value from a (possibly heterogeneous) struct.
 getField :: (Monad m, XmlRpcType a) => 
 	    String           -- ^ Field name
